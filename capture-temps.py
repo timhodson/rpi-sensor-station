81fd9e348dcc34417789579c7ec855377e9963fe
Tim Hodson <tim@timhodson.com>
2014-03-29 13:47:49 +0000

    A script to output values to CSV

---
+++ capture-temps.py
@@ -0,0 +1,49 @@
+__author__ = 'timhodson'
+# based on an example by http://www.seanlandsman.com/2013/02/the-raspberry-pi-and-wireless-rf-xrf.html?m=1
+import serial
+from time import sleep, gmtime, strftime
+import csv
+import sys
+from LLAP import LLAP
+
+DEVICE = '/dev/ttyAMA0'
+BAUD = 9600
+
+if len(sys.argv) < 2:
+    print "Exit: No filename given"
+    exit()
+
+outfile = sys.argv[1]
+ofh = open(outfile, 'w')
+csv_writer = csv.writer(ofh, dialect='Excel')
+
+llap = LLAP()
+
+print (strftime("%a, %d %b %Y %H:%M:%S: Starting\n", gmtime()))
+
+ser = serial.Serial(DEVICE, BAUD)
+while True:
+    print("%s: Checking..." % strftime("%a, %d %b %Y %H:%M:%S", gmtime()))
+    n = ser.inWaiting()
+    if n != 0:
+        msg = ser.read(n)
+        print("%s: %s" % (strftime("%a, %d %b %Y %H:%M:%S", gmtime()), msg))
+        for response in llap.get_responses(msg):
+            response.prepend(strftime("%a, %d %b %Y %H:%M:%S", gmtime()))
+            csv_writer.writerow(response)
+    sleep(1 * 60)
+
+
+
+# code flow
+# event based
+    # for each event
+    # log raw msg
+    # split message into components
+    # store messages by device and by event.
+    #
+# batt event
+
+# battlow event
+    # some sort of alert via twitter/sms/email?
+# temp reading event
