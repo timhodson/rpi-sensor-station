2a734fcd9413b0a51d7876edaad6adf767f34e74
Tim Hodson <tim@timhodson.com>
2014-03-30 19:21:58 +0100

    refined the event callback and added a firebase writing callback to the capture-temps.py script.

--- LLAP.py
+++ LLAP.py
@@ -4,7 +4,6 @@ __author__ = 'timhodson'
 # a python module to support llap
 # currently mainly the llap Thermister device Persona
 class LLAP:
-
     RESPONSES = {
         'AWAKE': 'Device is awake',
         'TMPA': 'temperature reading',
@@ -25,53 +24,84 @@ class LLAP:
     observers = []
 
     def __init__(self):
+        # might want to do something clever with detecting serial ports and what not?
         pass
 
-    def split_msg(self, msg):
-        # split the message up into bits
-        # there may be more than one llap response in a message
-        self.validate_msg(msg)
+    def split_response(self, response):
+        """Split the response up into bits
+
+        Keyword arguments:
+        response -- the string to split into bits.
+
+        Returns:
+        bits -- the bits of the response
+        """
+        self.validate_msg(response)
         bits = {
-            'raw': msg,
-            'a': msg[0],
-            'deviceId': msg[1:3]
+            'raw': response,
+            #'a': response[0],
+            'deviceId': response[1:3]
         }
         for key in self.RESPONSES.keys():
-            if key in msg:
-                bits['responseType'] = msg[3:3+len(key)]
-                bits['responseValue'] = msg[3+len(key):].rstrip("-")
+            if key in response:
+                bits['responseType'] = response[3:3 + len(key)]
+                bits['responseValue'] = response[3 + len(key):].rstrip("-")
         self.notify_observers(bits['responseType'], bits)
         return bits
 
     def get_responses(self, msg):
+        """Get the parsed responses out of an arbitrary message string.
+        You are assumed to have got the LLAP message yourself
+        by whatever means from the serial port
+
+        Keyword arguments:
+        msg -- a String which may contain LLAP responses from a device.
+
+        Returns:
+        responses -- a list of dictionaries for each response found in the message.
+        """
         responses = []
         if len(msg) % 12 != 0:
             print "'%s'" % msg
             raise Exception("message not divisible by 12")
-        msg_count = len(msg)/12
+        msg_count = len(msg) / 12
         s = 0
-        for i in range(1, msg_count+1):
-            e = i*12
-            responses.append(self.split_msg(msg[s:e]))
+        for i in range(1, msg_count + 1):
+            e = i * 12
+            responses.append(self.split_response(msg[s:e]))
             s += 12  # increment start ready for next loop
         return responses
 
     def build_request(self, devid, type):
-        if len(devid) > 2 or len(type) > 9:
-            raise Exception("invalid parameters to build_request")
+        self.validate_request(devid, type)
         request = '{0}{1}{2:-<9}'.format('a', devid, type)
         self.validate_msg(request)
         return request
 
     def validate_msg(self, msg):
         if len(msg) > 12:
+            # todo - raise some more specific Exceptions
             raise Exception("completed request is too long")
 
-    def register_observer(self, event,  observer):
+    def validate_request(self, d, t):
+        if len(d) > 2 or len(t) > 9:
+            raise Exception("invalid parameters to build_request")
+
+    def register_observer(self, event, observer):
+        """Register an observer to deal with each response found.
+        Each callback is self contained and cannot be chained with other callbacks.
+        For example: you cannot register a callback to add a timestamp
+        to every message and then expect other callbacks to also see that timestamp.
+
+        Keyword arguments:
+        event -- the type of message to react to
+        observer -- a reference to the callback to execute
+        """
         self.observers.append({event: observer})
 
     def notify_observers(self, event, data):
         for observer in self.observers:
-            if observer.keys()[0] == event:
-                observer.values()[0](event, data)
+            if observer.keys()[0] == event or observer.keys()[0] == 'ALL':
+                observer.values()[0](data)
+
 #ends
--- capture-temps.py
+++ capture-temps.py
@@ -4,11 +4,15 @@ import serial
 from time import sleep, gmtime, strftime, time
 import csv
 import sys
+import os
 from LLAP import LLAP
+from firebase import firebase
 
+# serial configuration
 DEVICE = '/dev/ttyAMA0'
 BAUD = 9600
 
+# output file generation
 if len(sys.argv) < 2:
     print "Exit: No filename given"
     exit()
@@ -17,7 +21,32 @@ outfile = sys.argv[1]
 ofh = open(outfile, 'w')
 csv_writer = csv.writer(ofh, dialect='excel')
 
+# firebase configuration
+if any([os.getenv('FIREBASE_TOKEN') is None, os.getenv('FIREBASE_EMAIL') is None]):
+    print "\nBoth FIREBASE_TOKEN and FIREBASE_EMAIL must be set in your OS environment"
+    exit(1)
+
+# connect to Firebase
+authentication = firebase.FirebaseAuthentication(os.getenv('FIREBASE_TOKEN'), os.getenv('FIREBASE_EMAIL'))
+fb = firebase.FirebaseApplication('https://rpi-sensor-network.firebaseio.com/', authentication)
+
+
+def csv_writer_callback(response):
+    response['time'] = time()
+    print response.values()
+    csv_writer.writerow(response.values())
+    ofh.flush()
+
+
+def firebase_writer_callback(data):
+    # write some data to firebase for temperature readings.
+    fb.put('/' + data['deviceId'], data['time'], data)
+
+# set up our llap callbacks
 llap = LLAP()
+llap.register_observer('ALL', csv_writer_callback)
+llap.register_observer('TMPA', firebase_writer_callback)
+
 
 print (strftime("%a, %d %b %Y %H:%M:%S: Starting\n", gmtime()))
 
@@ -28,10 +57,9 @@ while True:
     if n != 0:
         msg = ser.read(n)
         print("%s: %s" % (strftime("%a, %d %b %Y %H:%M:%S", gmtime()), msg))
-        for response in llap.get_responses(msg):
-            response['time'] = time()
-            print response.values()
-            csv_writer.writerow(response.values())
-            ofh.flush()
+
+        # this is all we need to call now that we have registered our callbacks!
+        llap.get_responses(msg)
+
     sleep(1 * 60)
 
