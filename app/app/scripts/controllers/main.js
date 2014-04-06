15919f2c8cd888a7d57473cabe2adf33965580b4
Tim Hodson <tim@timhodson.com>
2014-04-06 19:54:10 +0100

    Updated data fetching functions to use promises. Added buttons for different length of temp history

--- app/app/scripts/controllers/main.js
+++ app/app/scripts/controllers/main.js
@@ -1,11 +1,13 @@
 'use strict';
 
 angular.module('appApp')
-  .controller('MainCtrl', ['$scope', 'syncData', 'snapshot', 'dateFilter', function ($scope, syncData, snapshot, dateFilter) {
+  .controller('MainCtrl', ['$scope', 'syncData', 'snapshot', 'dateFilter', '$q', function ($scope, syncData, snapshot, dateFilter, $q) {
 
     $scope.ght = syncData('AB', 1);
+    $scope.ghtReadings = 12;
 
     $scope.lt = syncData('AA', 1);
+    $scope.ltReadings = 6;
 
     $scope.getDate = function (timestamp) {
       var date = new Date(parseFloat(timestamp) * 1000);
@@ -34,10 +36,12 @@ angular.module('appApp')
       //      }
       //  ]
       // }
+      var deferred = $q.defer();
       var chartData = {};
       chartData.series = ['temperature'];
       chartData.data = [];
       snapshot(sensor, dataPoints, 0, function (data) {
+        console.log('snapshot callback called');
         data.forEach(function (d) {
           var dp = {};
           var dateLabel = dateFilter(new Date(parseFloat(d.child('time').val()) * 1000), 'yyyy-MM-dd HH:mm:ss');
@@ -47,22 +51,26 @@ angular.module('appApp')
           dp.tooltip = dateLabel;
           chartData.data.push(dp);
         });
+        deferred.resolve(chartData);
       });
-      return chartData;
 
+      return deferred.promise;
     };
-    // 288 * 5 min = 24 hours
-    $scope.$watch(function () {
-      return $scope.ght;
-    }, function () {
-      $scope.chartData1 = $scope.getChartData('AB', 288); // taken every 5 mins
-    });
 
-    $scope.$watch(function () {
-      return $scope.lt;
-    }, function () {
-      $scope.chartData2 = $scope.getChartData('AA', 144); // taken every 10 mins
-    });
+    // 288 * 5 min = 24 hours
+    $scope.$watch('[ght, ghtReadings]', function () {
+      //$scope.chartData1 = {};
+      $scope.getChartData('AB', $scope.ghtReadings).then(function (data) {
+        $scope.chartData1 = data;
+      }); // taken every 5 mins
+    }, true);
+
+    $scope.$watch('[lt, ltReadings]', function () {
+      //$scope.chartData2 = {};
+      $scope.getChartData('AA', $scope.ltReadings).then(function(data){
+        $scope.chartData2 = data;
+      }); // taken every 10 mins
+    }, true);
 
     $scope.chartType = 'line';
     $scope.chartConfig1 = {
@@ -71,20 +79,22 @@ angular.module('appApp')
       click: function (d) {
         console.log(d);
       },
-      title: 'Temperature - last 24 hours from ' + dateFilter(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+      title: 'Temperature - last ' + ($scope.ghtReadings * 5 / 60) + ' hours from ' + dateFilter(new Date(), 'yyyy-MM-dd HH:mm:ss'),
       legend: {
         display: false,
         position: 'left'
-      }
+      },
+      height: '600px'
     };
     $scope.chartConfig2 = {
       labels: false,
       tooltips: true,
-      title: 'Temperature - last 24 hours from ' + dateFilter(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+      title: 'Temperature - last ' + ($scope.ltReadings * 10 / 60) + ' hours from ' + dateFilter(new Date(), 'yyyy-MM-dd HH:mm:ss'),
       legend: {
         display: false,
         position: 'left'
-      }
-    };
+      },
+      height: '600px'
+  };
 
   }]);
