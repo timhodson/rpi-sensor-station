8687566680ae51ab19d4bc605048a0411028d7e2
Tim Hodson <tim@timhodson.com>
2014-07-04 20:27:08 +0100

    completely rewritten to be actual unit tests!

--- test-llap.py
+++ test-llap.py
@@ -1,40 +1,123 @@
 __author__ = 'timhodson'
 
-from LLAP import LLAP
+from LLAP import LLAP, LlapException
 from pprint import pformat
-
-llap = LLAP()
-
-print "TEST: build some requests"
-print llap.build_request("AA", "TEMP")
-print llap.build_request("AA", "HELLO")
-
-print "TEST: trying some invalid requests"
-try:
-    print llap.build_request("AAB", "HELLO")
-except Exception:
-    print "failed as expected with deviceid AAB"
-
-try:
-    print llap.build_request("AB", "HELLOHELLO")
-except Exception:
-    print "failed as expected with request HELLOHELLO"
+import unittest
 
 
-def print_data(event, data):
-    print "EVENT %s TRIGGERED and passed DATA: %s" % (event, pformat(data))
+class TestLLAPFunctionality(unittest.TestCase):
+    def setUp(self):
+        self.llap = LLAP()
 
-print "TEST: Register an observer"
-llap.register_observer('TMPA', print_data)
+    def test_build_request(self):
+        self.assertEqual(self.llap.build_request('AA', 'TEMP'), 'aAATEMP-----')
+        self.assertEqual(self.llap.build_request('AA', 'HELLO'), 'aAAHELLO----')
+        self.assertRaises(LlapException, self.llap.build_request, 'AAB', 'HELLO')
+        self.assertRaises(LlapException, self.llap.build_request, 'AB', 'HELLOHELLO')
 
-print "TEST: different types of response with values and hyphen padding"
+    def test_register_observer(self):
+        self.assertEqual(len(self.llap.observers), 0)
 
-print llap.get_responses("aABBATT2.67-")
-print llap.get_responses("aABAWAKE----")
-print llap.get_responses("aABSLEEPING-")
+        # note that we test with a fictitious event name
+        # These tests run in parallel and don't seem to be run with their own llap object so events get fired!
+        self.llap.register_observer('TMP', self.print_data)
+        self.assertEqual(len(self.llap.observers), 1)
+        self.assertDictEqual(self.llap.observers[0], {'TMP': self.print_data})
 
-print "TEST: process multiple responses in one message"
-print pformat(llap.get_responses("aABTMPA20.23aABAWAKE----aABBATT2.76-aABSLEEPING-"))
+        self.llap.unregister_observer('TMP', self.print_data)
+        self.assertEqual(len(self.llap.observers), 0)
 
-print "TEST: call get_responses without capturing return value - just relying on event to trigger something "
-llap.get_responses("aABTMPA23.13")
+    def test_get_responses(self):
+        data = self.llap.get_responses("aABBATT2.67-")
+        self.assertDictEqual(self.update_time(data[0]),
+                             {'responseType': 'BATT', 'raw': 'aABBATT2.67-', 'responseValue': '2.67', 'deviceId': 'AB',
+                              'time': 0})
+
+        data = self.llap.get_responses("aABBATTLOW--")
+        self.assertDictEqual(self.update_time(data[0]),
+                             {'responseType': 'BATT', 'raw': 'aABBATTLOW--', 'responseValue': 'LOW', 'deviceId': 'AB',
+                              'time': 0})
+
+        data = self.llap.get_responses("aABAWAKE----")
+        self.assertDictEqual(self.update_time(data[0]),
+                             {'responseType': 'AWAKE', 'raw': 'aABAWAKE----', 'responseValue': '', 'deviceId': 'AB',
+                              'time': 0})
+
+        data = self.llap.get_responses("aABSLEEPING-")
+        self.assertDictEqual(self.update_time(data[0]),
+                             {'responseType': 'SLEEPING', 'raw': 'aABSLEEPING-', 'responseValue': '', 'deviceId': 'AB',
+                              'time': 0})
+
+    def test_multiple_responses(self):
+        data = self.llap.get_responses("aABTMPA20.24aABAWAKE----aABBATT2.76-aABSLEEPING-")
+        data = [self.update_time(x) for x in data]
+        expected = [{'deviceId': 'AB',
+                     'raw': 'aABTMPA20.24',
+                     'responseType': 'TMPA',
+                     'responseValue': '20.24',
+                     'time': 0},
+                    {'deviceId': 'AB',
+                     'raw': 'aABAWAKE----',
+                     'responseType': 'AWAKE',
+                     'responseValue': '',
+                     'time': 0},
+                    {'deviceId': 'AB',
+                     'raw': 'aABBATT2.76-',
+                     'responseType': 'BATT',
+                     'responseValue': '2.76',
+                     'time': 0},
+                    {'deviceId': 'AB',
+                     'raw': 'aABSLEEPING-',
+                     'responseType': 'SLEEPING',
+                     'responseValue': '',
+                     'time': 0}]
+        self.assertEqual(data, expected)
+
+    def test_messages_of_incorrect_length(self):
+        """
+        The messages should still get completed even if one of the messages was not read completely from serial input.
+        (all llap messages should start with a lower case 'a' and run for 12 chars.
+        """
+        data = self.llap.get_responses("--aZZTMPA20.23aABAWAKE----aABBATT2.76-aABSLEEPING-aZZ-----")
+        data = [self.update_time(x) for x in data]
+        expected = [{'deviceId': 'ZZ',
+                     'raw': 'aZZTMPA20.23',
+                     'responseType': 'TMPA',
+                     'responseValue': '20.23',
+                     'time': 0},
+                    {'deviceId': 'AB',
+                     'raw': 'aABAWAKE----',
+                     'responseType': 'AWAKE',
+                     'responseValue': '',
+                     'time': 0},
+                    {'deviceId': 'AB',
+                     'raw': 'aABBATT2.76-',
+                     'responseType': 'BATT',
+                     'responseValue': '2.76',
+                     'time': 0},
+                    {'deviceId': 'AB',
+                     'raw': 'aABSLEEPING-',
+                     'responseType': 'SLEEPING',
+                     'responseValue': '',
+                     'time': 0}]
+        self.assertEqual(data, expected)
+
+    def update_time(self, d):
+        if d.get('time'):
+            d['time'] = 0
+        return d
+
+    def print_data(self, data):
+        print "EVENT %s TRIGGERED and passed DATA: %s" % (data['responseType'], pformat(data))
+
+def suite1():
+    suite = unittest.TestSuite()
+    suite.addTest(TestLLAPFunctionality("test_build_request"))
+    suite.addTest(TestLLAPFunctionality("test_register_observer"))
+    suite.addTest(TestLLAPFunctionality("test_get_responses"))
+    suite.addTest(TestLLAPFunctionality("test_multiple_responses"))
+    suite.addTest(TestLLAPFunctionality("test_messages_of_incorrect_length"))
+    return suite
+
+
+unittest.TextTestRunner(verbosity=2).run(suite1())
